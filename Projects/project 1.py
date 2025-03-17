632f17712f8dbd6487980db95d43d923e05b6b6d
Madison-taylor <madisontaylor@seattleacademy.org>
2025-03-17 16:17:50 +0000

    turing in project 1

---
+++ Projects/project 1.py
@@ -0,0 +1,19 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+stage.set_background("park")
+
+q1=codesters.Square (100,100,200 ,'LightBlue')
+q1=codesters.Square (-100,100,200, 'CornflowerBlue')
+q1=codesters.Square (-100,-100,200, 'RoyalBlue')
+q1=codesters.Square (100,-100,200, 'MediumSlateBlue')
+
+s1=codesters.Sprite("gymnast 2",100,100)
+s2=codesters.Sprite("baking 2",-100,-100)
+s2.set_size(0.5)
+s3=codesters.Sprite("panda 2",100,-100)
+s3.set_size(0.5)
+s4=codesters.Sprite("cardinal",-100,100)
