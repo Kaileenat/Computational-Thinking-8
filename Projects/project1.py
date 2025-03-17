15897c76b4d31ce1c1e59eedf0b6a430c69273d2
Kaileenat <kaileemorenohernandez@seattleacademy.org>
2025-03-17 16:04:30 +0000

    test

---
+++ Projects/project1.py
@@ -0,0 +1,14 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+stage.set_background("winter")
+
+
+q1 = codesters.Square (100, 100, 200, 'pink')
+q2 = codesters.Square (-100, 100, 200, 'AliceBlue')
+q3 = codesters.Square (-100, -100, 200, 'LightCyan')
+q4 = codesters. Square (100, -100, 200, 'MintCream')
