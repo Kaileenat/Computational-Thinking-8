ff09110cbcb1392808c74755e22f705215dccea3
Kaileenat <kaileemorenohernandez@seattleacademy.org>
2025-03-31 21:44:19 +0000

    Project 1 submission

---
+++ Projects/new porject.py
@@ -0,0 +1,19 @@
+giraffe_points = 0
+dog_points = 0
+
+
+answer = input ("Which one you like better A) zoos, or B) pet stores?")
+if answer == "A":
+giraffe_points += 1
+    elif answer == "B" :
+    dog_points += 1
+
+
+    answer = input ("do you like A) outdoor pets, or B) indoor pets?")
+    if answer == "A"
+    giraffe_points += 1
+    elif answer == "B" :
+    dog_points += 1
+
+
+    
