4b4f3be2c7f1008bcf557eedfdb3ebf6b118b396
Kaileenat <kaileemorenohernandez@seattleacademy.org>
2025-04-08 16:21:31 +0000

    sumbit

---
+++ Projects/project 3.py
@@ -0,0 +1,22 @@
+import turtle 
+
+t = turtle.Turtle ()
+
+t.goto(100, 100)
+t.color("cyan")
+
+for i in range(1):
+    t .forward(3)
+    t.left(80)
+    t = turtle.Turtle ()
+    t = turtle.Turtle ()
+
+t.goto(100, 100)
+t.color("pink")
+
+for i in range(3):
+    t .forward(100)
+    t.left(70)
+    t = turtle.Turtle ()
+    
+turtle.exitonclick()
